a533ccba44f0d91f6dd0b4cab46f138e2438eb1f
heyongqi10 <heyongqi10@jd.com>
2024-12-16 13:34:42 +0800

    feat: 组件展位

--- packages/design/index.ts
+++ packages/design/index.ts
@@ -1,6 +1,29 @@
 import "./src/styles/index.css";
+
+import DollMachine from "./src/components/DollMachine";
 import GiftBox from "./src/components/GiftBox";
+import GiftRain from "./src/components/GiftRain";
+import GuessGift from './src/components/GuessGift';
+import Hiteggs from './src/components/Hiteggs';
+import LottoRoll from './src/components/LottoRoll';
+import LuckShake from './src/components/LuckShake';
+import Marquee from './src/components/Marquee';
+import Scratchcard from './src/components/Scratchcard';
+import ShakeDice from './src/components/ShakeDice';
+import SquareNine from './src/components/SquareNine';
+import TurnTable from './src/components/TurnTable';
 
 export {
+  DollMachine,
   GiftBox,
+  GiftRain,
+  GuessGift,
+  Hiteggs,
+  LottoRoll,
+  LuckShake,
+  Marquee,
+  Scratchcard,
+  ShakeDice,
+  SquareNine,
+  TurnTable
 };
---
+++ packages/design/src/components/Marquee.tsx
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect } from "react";
+import cls from '@xxhls/classnames'
+
+const Marquee: React.FC = () => {
+
+    const contentRef = useRef<HTMLDivElement>(null);
+    const activeRef = useRef<boolean>(false);
+
+    const init = () => {
+        activeRef.current = false;
+    };
+
+    const boxClickHandler = () => {
+        if (activeRef.current) {
+            return;
+        }
+        activeRef.current = true;
+        let transitionFlag = true;
+        let contentDom = contentRef.current!;
+        contentDom.addEventListener("webkitTransitionEnd", function (e) {
+            if (e.target === e.currentTarget && transitionFlag) {
+                transitionFlag = false;
+                removeListener();
+            }
+        });
+    }
+
+    const removeListener = () => {
+        contentRef.current!.removeEventListener("webkitTransitionEnd", removeListener);
+    }
+
+
+    useEffect(() => {
+        return () => {
+            removeListener();
+        };
+    }, []);
+
+    return (
+        <>
+            <div className="nutbig-giftBox nutbig-giftBox_wrapper">
+                <div className="nutbig-giftBox_content" ref={contentRef} onClick={boxClickHandler}>
+                    <div className={cls("nutbig-giftBox_cover", {
+                        "nutbig-giftBox_cover--active": activeRef.current
+                    })}></div>
+                    <div className="nutbig-giftBox_box"></div>
+                    <div className={cls("nutbig-giftBox_line", {
+                        "nutbig-giftBox_line--active": activeRef.current
+                    })}></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Marquee;
